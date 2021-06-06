d2d2fd953fe9eaef8b17fc394d68f1187eada979
Miguel Nischor <miguelnischor@gmail.com>
2021-06-06 19:28:01 -0400

    Initial implementation

---
+++ Script/Create_Database.sql
@@ -0,0 +1,7 @@
+-- SimpleERP ERP System
+-- Copyright (C) 2021 Miguel Nischor
+--
+-- File: Create_Database.sql
+-- Description: SQL script for database creation, with UTF-8 collation
+-- Author: Miguel Nischor <miguel@nischor.com.br>
+CREATE DATABASE IF NOT EXISTS `serp` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
